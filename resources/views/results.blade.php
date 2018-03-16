7732546e2d63bd39008cfcfb7dbbfe3c348bdfce
disastersystem <robin.vigdal.bekkevold@gmail.com>
2018-03-16 03:06:34 +0100

    no message

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -19,95 +19,82 @@
         </style>
     </head>
     <body>
-        <!-- <table>
-            @foreach ($answers as $answer)
-                <tr>
-                    <td>{{ $answer->id }}</td>
-                    <td>{{ $answer->image }}</td>
-                    <td>{{ $answer->answer }}</td>
-                    <td>{{ $answer->subject }}</td>
-                </tr>
-            @endforeach
-        </table> -->
-
         <?php
+            $images = [
+                'final01_d2_l1' => 1,
+                'final01_d2_l2' => 2,
+                'final01_d2_l3' => 3,
+                'final01_d2_l4' => 4,
+                'final01_d2_l5' => 5,
+                // two circle lamps
+                'final02_d2_l1' => 6,
+                'final02_d2_l2' => 7,
+                'final02_d2_l3' => 8,
+                'final02_d2_l4' => 9,
+                'final02_d2_l5' => 10,
+                // red winter barn
+                'final07_d2_l1' => 11,
+                'final07_d2_l2' => 12,
+                'final07_d2_l3' => 13,
+                'final07_d2_l4' => 14,
+                'final07_d2_l5' => 15,
+                // food
+                'final20_d2_l1' => 16,
+                'final20_d2_l2' => 17,
+                'final20_d2_l3' => 18,
+                'final20_d2_l4' => 19,
+                'final20_d2_l5' => 20,
+                // sunflower
+                'final16_d2_l1' => 21,
+                'final16_d2_l2' => 22,
+                'final16_d2_l3' => 23,
+                'final16_d2_l4' => 24,
+                'final16_d2_l5' => 25,
+                // peacock
+                'final09_d2_l1' => 26,
+                'final09_d2_l2' => 27,
+                'final09_d2_l3' => 28,
+                'final09_d2_l4' => 29,
+                'final09_d2_l5' => 30,
+                // sunflower
+                'final21_d2_l1' => 31,
+                'final21_d2_l2' => 32,
+                'final21_d2_l3' => 33,
+                'final21_d2_l4' => 34,
+                'final21_d2_l5' => 35,
+                // grass and water
+                'final06_d2_l1' => 36,
+                'final06_d2_l2' => 37,
+                'final06_d2_l3' => 38,
+                'final06_d2_l4' => 39,
+                'final06_d2_l5' => 40,
+                // grass and water
+                'final08_d2_l1' => 41,
+                'final08_d2_l2' => 42,
+                'final08_d2_l3' => 43,
+                'final08_d2_l4' => 44,
+                'final08_d2_l5' => 45,
+                // grass and water
+                'final18_d2_l1' => 46,
+                'final18_d2_l2' => 47,
+                'final18_d2_l3' => 48,
+                'final18_d2_l4' => 49,
+                'final18_d2_l5' => 50,
+                // grass and water
+                'final19_d2_l1' => 51,
+                'final19_d2_l2' => 52,
+                'final19_d2_l3' => 53,
+                'final19_d2_l4' => 54,
+                'final19_d2_l5' => 55
+            ];
+
             $categories = ['bad' => 1, 'poor' => 2, 'fair' => 3, 'good' => 4, 'excellent' => 5];
-
-    $images = [
-        'final01_d2_l1' => 1,
-        'final01_d2_l2' => 2,
-        'final01_d2_l3' => 3,
-        'final01_d2_l4' => 4,
-        'final01_d2_l5' => 5,
-        // two circle lamps
-        'final02_d2_l1' => 6,
-        'final02_d2_l2' => 7,
-        'final02_d2_l3' => 8,
-        'final02_d2_l4' => 9,
-        'final02_d2_l5' => 10,
-        // red winter barn
-        'final07_d2_l1' => 11,
-        'final07_d2_l2' => 12,
-        'final07_d2_l3' => 13,
-        'final07_d2_l4' => 14,
-        'final07_d2_l5' => 15,
-        // food
-        'final20_d2_l1' => 1,
-        'final20_d2_l2' => 1,
-        'final20_d2_l3' => 1,
-        'final20_d2_l4' => 1,
-        'final20_d2_l5' => 1,
-        // sunflower
-        'final16_d2_l1' => 1,
-        'final16_d2_l2' => 1,
-        'final16_d2_l3' => 1,
-        'final16_d2_l4' => 1,
-        'final16_d2_l5' => 1,
-        // peacock
-        'final09_d2_l1' => 1,
-        'final09_d2_l2' => 1,
-        'final09_d2_l3' => 1,
-        'final09_d2_l4' => 1,
-        'final09_d2_l5' => 1,
-        // sunflower
-        'final21_d2_l1' => 1,
-        'final21_d2_l2' => 1,
-        'final21_d2_l3' => 1,
-        'final21_d2_l4' => 1,
-        'final21_d2_l5' => 1,
-        // grass and water
-        'final06_d2_l1' => 1,
-        'final06_d2_l2' => 1,
-        'final06_d2_l3' => 1,
-        'final06_d2_l4' => 1,
-        'final06_d2_l5' => 1,
-        // grass and water
-        'final08_d2_l1' => 1,
-        'final08_d2_l2' => 1,
-        'final08_d2_l3' => 1,
-        'final08_d2_l4' => 1,
-        'final08_d2_l5' => 1,
-        // grass and water
-        'final18_d2_l1' => 1,
-        'final18_d2_l2' => 1,
-        'final18_d2_l3' => 1,
-        'final18_d2_l4' => 1,
-        'final18_d2_l5' => 1,
-        // grass and water
-        'final19_d2_l1' => 1,
-        'final19_d2_l2' => 1,
-        'final19_d2_l3' => 1,
-        'final19_d2_l4' => 1,
-        'final19_d2_l5' => 1
-    ];
-
 
             foreach ($answers as $answer) {
                 $path_parts = pathinfo($answer->image);
 
                 foreach ($images as $image => $key) {
                     if ($image == $path_parts['filename']) {
-                        // echo $path_parts['filename'] . " " . $key . "<br>";
                         $path_parts['filename'] = $key;
                     }
                 }
